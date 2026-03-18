2ead5f49ed19f30d53a7d88e3c8367d221a6560f
Shivam Gravity <shivamgravity@gmail.com>
2026-03-18 17:14:22 +0530

    updated rag query function

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,15 +1,12 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
-
 
 def create_rag_chain(pdf_path):
     # 1. Load PDF
@@ -39,7 +36,8 @@ def create_rag_chain(pdf_path):
 
     # 🔥 NEW RAG FUNCTION (no chains)
     def rag_query(question):
-        docs = retriever.get_relevant_documents(question)
+        docs = retriever.invoke(question)
+
         context = "\n\n".join([doc.page_content for doc in docs])
 
         prompt = f"""
