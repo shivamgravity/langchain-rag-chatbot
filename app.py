8d16bc42b6b4dd96db0cc7c9143e3418692cae09
Shivam Gravity <shivamgravity@gmail.com>
2026-03-18 17:15:18 +0530

    updated the streamlit app codebase

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from rag_pipeline import create_rag_chain
 
-st.title("📚 RAG Chatbot")
+st.set_page_config(page_title="RAG Chatbot", layout="centered")
 
+st.title("📚 Chat with Your PDF")
+
+# Upload PDF
 uploaded_file = st.file_uploader("Upload a PDF", type="pdf")
 
 if uploaded_file:
@@ -11,11 +14,14 @@ if uploaded_file:
 
     st.success("PDF uploaded successfully!")
 
-    if "chain" not in st.session_state:
-        st.session_state.chain = create_rag_chain("temp.pdf")
+    # Create chain once
+    if "rag" not in st.session_state:
+        st.session_state.rag = create_rag_chain("temp.pdf")
 
+    # Chat input
     query = st.text_input("Ask a question:")
 
     if query:
-        response = st.session_state.chain.run(query)
+        response = st.session_state.rag(query)
+        st.write("### 🤖 Answer:")
         st.write(response)
